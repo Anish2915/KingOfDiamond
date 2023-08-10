697f64faa3a92bec12fd776db6a21ba288200b58
ANISH KUMAR <98650602+Nishu2915@users.noreply.github.com>
2023-08-10 06:34:27 +0530

    Your message here

--- commit_and_push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Automating Git Commit and Push
-
-git add .                             :: Stage all changes
-git commit -m "Your message here"     :: Commit changes
-git push origin main                  :: Push changes to remote
-
-echo Git commit and push complete
